255dfb9ac93580508d2531ad9982237a45968757
mooplol <niklas.m.banck@gmail.com>
2024-11-03 10:39:20 +0100

    Added report

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_characters = count_characters(text)
+    sorted_characters = sort_characters(num_characters)
+    create_report(num_words,sorted_characters)
     
-
-    test = count_characters(text)
-    #print(test)
-    print(f"{num_words} words found in the document")
-    print(num_characters)
+    
+    
 
 def get_num_words(text):
     words = text.split()
@@ -29,14 +28,28 @@ def count_characters(text):
         else:
             dictionary_character_count[txt] += 1
 
-        
-        
-
-
-        
     return dictionary_character_count
 
 
+def sort_characters(num_characters):
+    num_characters_list = list(num_characters.items())
+    num_characters_list.sort(reverse=True, key=lambda num_characters_list: num_characters_list[1])
+    
+    
+
+    return num_characters_list
+
+def create_report(num_words, sorted_characters):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    for char in sorted_characters:
+        if char[0].isalpha():
+            print(f"The '{char[0]}' character was found {char[1]} times")
+
+    print("--- End report ---")
+
+    return None
+        
 
 main()
 
